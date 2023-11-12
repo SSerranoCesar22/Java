6971d213d7a4452fa35b54dadb76e4723e3c02f9
SergioSerranoo <sergioserranocesar@gmail.comm>
2023-11-12 12:52:44 +0100

    add conversion

---
+++ Conversion/byte to double/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		byte num = 12;
+		System.out.println("byte:"+num);
+		System.out.println("short:"+(short) num );
+		System.out.println("int:"+(int) num );
+		System.out.println("long:"+(long) num );
+		System.out.println("float:"+(float) num );
+		System.out.println("double: "+ (double) num);
+		
+		
+		
+	}
+
+}
---
+++ Conversion/convert to data/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		double num = 1234452323;
+		System.out.println("double:"+num);
+		System.out.println("float:"+(float) num );
+		System.out.println("long:"+(long) num );
+		System.out.println("int:"+(int) num );
+		System.out.println("short:"+(short) num );
+		System.out.println("byte:"+(byte) num );
+	}
+
+}
